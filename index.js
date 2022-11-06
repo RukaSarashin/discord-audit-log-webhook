cc26474aa243076f5198bbee3d08c7dd6c17bd2b
Tiogui <76832856+Tiogui@users.noreply.github.com>
2022-11-06 13:53:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,3 @@ client.on("ready", ()=>{
   console.log(`${client.user.tag} is now Online! Prefix: ${client.config.prefix}`.bgGreen);
   //client.user.setActivity("Hello world", {type: "STREAMING", url: "https://twitch.tv/#"})
 })
-
-require("./logger")(client);
-//const logger = require("./logger");
-//logger(client)
